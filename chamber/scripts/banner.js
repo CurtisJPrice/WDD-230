11766eb4c97545e653ffe362381ab624aac05271
Your Name <you@example.com>
2024-02-15 22:15:50 -0800

    week06fixes

---
+++ chamber/scripts/banner.js
@@ -0,0 +1,35 @@
+const closeBtn = document.getElementById("close");
+const displayBanner = document.querySelector(".banner");
+
+closeBtn.addEventListener("click", hideBanner)
+
+function hideBanner(){
+    displayBanner.style.display = "none";
+}
+
+
+
+const currenDate = new Date();
+
+const dayOfWeek = currenDate.getDay();
+
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const currentDayOfWeek = daysOfWeek[dayOfWeek];
+
+if(currentDayOfWeek === 'Monday'){
+
+    displayBanner.style.display = "block";
+}
+else if(currentDayOfWeek === 'Tuesday'){
+
+    displayBanner.style.display = "block";
+}
+else if(currentDayOfWeek === 'Wednesday'){
+    
+    displayBanner.style.display = "block";
+}
+else{
+    displayBanner.style.display = "none";
+
+}
